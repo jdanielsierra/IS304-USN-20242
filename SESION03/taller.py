b1c2c3d6b61dd0c81ee7d7fb33d8671a6a62a027
jdanielsierra <jdanielsierra@unimonserrate.edu.co>
2024-08-28 19:37:37 -0500

    Se crea la Clase junto con la reserva de cada uno de los atributos, también se crean los métodos

--- SESION03/taller.py
+++ SESION03/taller.py
@@ -3,3 +3,39 @@ Crear un programa que permita crear objetos de la clase CuentaBancaria, cuyos at
 Aplicar encapsulamiento y definir los métodos apropiados para controlar y gestionar los atributos de los objetos creados.
 Utilizar un menu para las diferentes opciones, tales como aperturaCta, consignar, retirar y transferencia entre otros.
 '''
+class CuentaBancaria:
+def __init__(self, numeroCta, nombreCliente, saldoCta, fechaApertura, ultimoRetiro, ultimaConsignacion):
+  self.__numeroCta = numeroCta
+  self.__nombreCliente = nombreCliente
+  self.__saldoCta = saldoCta
+  self.__fechaApertura =fechaApertura
+  self.__ultuimoRetiro = ultimoRetiro
+  self.__ultimaCosignacion = ultimaConsignacion
+
+def set(self, numeroCta, nombreCliente, saldoCta, fechaApertura, ultimoRetiro, ultimaConsignacion)
+  self.__numeroCta = numeroCta
+  self.__nombreCliente = nombreCliente
+  self.__saldoCta = saldoCta
+  self.__fechaApertura = fechaApertura
+  self.__ultuimoRetiro = ultimoRetiro
+  self.__ultimaCosignacion = ultimaConsignacion
+
+
+def set_numeroCta(self, x):
+  self.__numeroCta = x
+
+def set_nombreCliente(self, x):
+  self.__nombreCliente = x
+  
+def set_saldoCta(self, x):
+  self.__saldoCta = x
+
+def set_fechaApertura(self, x):
+  self.__fechaApertura = x
+
+def set_ultimoRetiro(self, x):
+  self.__ultimoRetiro = x
+
+def set_ultimaConsignacion(self, x):
+  self.__ultimaConsignacion = x
+  
